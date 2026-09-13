8c811b4e8259bcd6449e7bfae6fa92ebcace78ff
ADB-XXV <anindithadbaiju@gmail.com>
2026-09-13 22:00:44 +0530

    page1ver3

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,9 +7,7 @@ export default function Navbar() {
 
   return (
     <header className="navbar">
-      <a href="/" className="logo">
-        <img src="/assets/tathva.png" alt="Tathva '26 NIT Calicut" />
-      </a>
+      
 
       <nav className="nav-actions" aria-label="Event navigation">
         <a href="/prizes">Prizes</a>
--- src/app/components/pg1.jsx
+++ src/app/components/pg1.jsx
@@ -3,7 +3,14 @@
 export default function Pg1() {
   return (
     <main className="hero">
-      <img src="/assets/hand-left.png" className="decor decor-left" alt="" />
+      <a href="/" className="logo">
+        <img src="/assets/tathva.png" alt="Tathva '26 NIT Calicut" />
+      </a>
+      <div className="hand-glow glow-left"></div>
+<img src="/assets/hand-left.png" className="decor decor-left" alt="" />
+      <div className="hand-glow glow-right"></div>
+      <div className="hand-glow glow-right"></div>
+<img src="/assets/hand-right.png" className="decor decor-right" alt="" />
       <img src="/assets/hand-right.png" className="decor decor-right" alt="" />
 
       <section className="hero-content">
