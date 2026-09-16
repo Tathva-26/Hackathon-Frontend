a14c156fc66eb45b9389189f8c6265a393f65332
souparnikavp17 <souparnikavp17@gmail.com>
2026-09-16 23:50:30 +0530

    Add Lenis smooth scrolling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,32 @@
 import { Anton, Inter, Press_Start_2P, Roboto } from "next/font/google";
 import Navbar from "./components/Navbar";
 import ClientProviders from "./components/ClientProviders";
+import SmoothScroll from "./components/SmoothScroll";
 import "./globals.css";
 
-const anton = Anton({ weight: "400", subsets: ["latin"], variable: "--font-anton" });
-const inter = Inter({ weight: ["400","500","600"], subsets: ["latin"], variable: "--font-inter" });
-const pressStart = Press_Start_2P({ weight: "400", subsets: ["latin"], variable: "--font-press-start" });
-const roboto = Roboto({ weight: ["400","500","600"], subsets: ["latin"], variable: "--font-roboto" });
+const anton = Anton({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-anton",
+});
+
+const inter = Inter({
+  weight: ["400", "500", "600"],
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const pressStart = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-press-start",
+});
+
+const roboto = Roboto({
+  weight: ["400", "500", "600"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
 
 export const metadata = {
   title: "TatHack '26 | Tathva NIT Calicut",
@@ -20,9 +40,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${anton.variable} ${inter.variable} ${pressStart.variable} ${roboto.variable}`}>
+    <html
+      lang="en"
+      className={`${anton.variable} ${inter.variable} ${pressStart.variable} ${roboto.variable}`}
+    >
       <body>
         <ClientProviders>
+          <SmoothScroll />
           <Navbar />
           {children}
         </ClientProviders>
