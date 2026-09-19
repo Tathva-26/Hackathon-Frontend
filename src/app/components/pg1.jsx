b0789adaebf8754d254a72dfee5eb34b003b8076
nmngit <neerajmnair2007@gmail.com>
2026-09-19 19:23:02 +0530

    Add view brochure

--- src/app/components/pg1.jsx
+++ src/app/components/pg1.jsx
@@ -279,6 +279,12 @@ export default function Pg1() {
                   REGISTER NOW
                 </button>
                 <button className={styles.button}>SEE SCHEDULE</button>
+                <button
+                  className={styles.button}
+                  onClick={() => window.open('https://drive.google.com/file/d/1KYHz6SLjkVGrVMKwMLxXaYrxqvZnnBr-/view', '_blank', 'noopener,noreferrer')}
+                >
+                  VIEW BROCHURE
+                </button>
               </div>
             </div>
           </div>
